de0f890404f6b29047e824624efd92f64cf84ea1
schaefer-rex <86406497+schaefer-rex@users.noreply.github.com>
2021-06-24 16:32:15 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1877753406:AAG-MOxXGVYZjiBBerWLHxHgtjJL1zZWqwU"
+  APP_ID = 1957706
+  API_HASH = "b1fc05cb6e99b67c6fca96538c6d7e7e"
+  AUTH_CHANNEL = -1001322070077
